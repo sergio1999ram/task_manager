6b6faf890169a7c848373ec28fbea5a9bc8b373b
sergio1999ram <sramirezpch@gmail.com>
2021-03-12 14:07:34 -0500

    CSRF config

--- app_config.py
+++ app_config.py
@@ -1,8 +1,12 @@
 import pymongo
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
+csrf = CSRFProtect()
 db = pymongo.MongoClient(
     'mongodb+srv://admin:admin@cluster0.xddvb.mongodb.net/task-manager?retryWrites=true&w=majority')
 db.client.test
 
+
 app = Flask(__name__)
+csrf.init_app(app)
