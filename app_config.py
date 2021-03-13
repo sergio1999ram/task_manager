f909f31fbd3c7d89d9703e4f644404db7bba965b
sergio1999ram <sramirezpch@gmail.com>
2021-03-13 13:58:13 -0500

    mk

--- app_config.py
+++ app_config.py
@@ -3,8 +3,9 @@ import os
 import pymongo
 from dotenv import load_dotenv
 from flask import Flask
-from flask_wtf.csrf import CSRFProtect
+from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_wtf.csrf import CSRFProtect
 
 load_dotenv('.env')
 
@@ -12,12 +13,12 @@ app = Flask(__name__)
 csrf = CSRFProtect()
 login_manager = LoginManager()
 
+bcrypt = Bcrypt(app)
 csrf.init_app(app)
 login_manager.init_app(app)
 login_manager.login_view = "login"
 
-client = pymongo.MongoClient(
-    'mongodb+srv://admin:admin@cluster0.xddvb.mongodb.net/task-manager?retryWrites=true&w=majority')
+client = pymongo.MongoClient('DATABASE_URL')
 db = client.test
 
 app.config['SECRET_KEY'] = os.getenv('SECRETKEY')
